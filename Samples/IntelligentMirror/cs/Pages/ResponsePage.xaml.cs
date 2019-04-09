bc52a095dc197e34675919895277e220496a5b14
Piotr Ładoński <piolado@gmail.com>
2019-04-10 01:48:11 +0300

    [Pre] Raw Azure request

--- Samples/IntelligentMirror/cs/Pages/ResponsePage.xaml.cs
+++ Samples/IntelligentMirror/cs/Pages/ResponsePage.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using IntelligentMirror.Models;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +26,7 @@ namespace IntelligentMirror.Pages
     /// </summary>
     public sealed partial class ResponsePage : Page
     {
+        //TODO: Provide subscription key and uri of Microsoft Azure Face API service (given by a person conducting the workshop)
         private AzureConnector azure = new AzureConnector("<SUBSCRIPTION KEY>", @"<BASE URI>/vision/v1.0");
         public ResponsePage()
         {
@@ -41,12 +43,17 @@ namespace IntelligentMirror.Pages
                 using (IRandomAccessStream stream = await passedFile.OpenAsync(FileAccessMode.Read))
                 {
                     BitmapImage bitmap = new BitmapImage();
-                    bitmap.SetSource(stream);
+                    //TODO: (1) Streams can be used only once, so you have to Clone Stream here, to be able to use it later again
+                    bitmap.SetSource(stream??);
                     Img.Source = bitmap;
 
 
                     Img.RenderTransformOrigin = new Point(0.5, 0.5);
                     Img.RenderTransform = new ScaleTransform { ScaleX = -1 };
+                    //TODO: (2) Make analysis request to Microsoft Azure using data of the image. Where is it stored?
+                    string jsonResponse = await azure.MakeAnalysisRequest(??);
+                    //TODO: (3) Display somewhere received raw data, ex. in one of the TextBoxes
+                    //Your code here
                 }
             }
         }
