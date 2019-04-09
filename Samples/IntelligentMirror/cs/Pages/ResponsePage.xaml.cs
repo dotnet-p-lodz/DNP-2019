d4d9d18b9ffa58c4cc6ee3a83be1b96b44f2d5cc
Piotr Ładoński <piolado@gmail.com>
2019-04-10 02:51:43 +0300

    [Pre] Json parsing

--- Samples/IntelligentMirror/cs/Pages/ResponsePage.xaml.cs
+++ Samples/IntelligentMirror/cs/Pages/ResponsePage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -50,9 +50,40 @@ namespace IntelligentMirror.Pages
                     Img.RenderTransformOrigin = new Point(0.5, 0.5);
                     Img.RenderTransform = new ScaleTransform { ScaleX = -1 };
                     string jsonResponse = await azure.MakeAnalysisRequest(stream);
-                    ResponseBox.Text = jsonResponse;
+                    //TODO: (1) Face can be created From Json by providing JSON string as argument. There can be several faces, but now just pick First Or Default one.
+                    //Default means that face variable will be null if there are no faces on the photo.
+                    Face face = Face.??.??;
+                    //TODO: (2) Check, if there are any faces on the photo
+                    if (??)
+                    {
+                        //TODO: (3) Here use functions which will Setup Response and Setup Caption textboxes. Give them face as the argument.
+                    }
+                    else
+                    {
+                        //TODO: (4) If there are no faces, provide some default text for the user in both textboxes, ex. "No faces found"
+                    }
                 }
             }
+        }
+
+        private void SetupResponse(Face face)
+        {
+            //TODO: (5) Extract Age, Gender and Glasses information from face.FaceAttributes and add them to the response text.
+            string response = $"Age: {16 /*replace this!*/}\n";
+            response += $"Glasses: {false /*replace this!*/}\n";
+            ResponseBox.Text = response;
+        }
+
+        private void SetupCaption(Face face)
+        {
+            string caption = $"{face.FaceAttributes.Age}-year-old ";
+
+            //TODO: (6) Extract Gender and Glasses information, but present them in nice, human-readable way
+            //Ex. "22-year-old man wearing glasses" or "19-year-old woman".
+            //Try taking gender and age into account to write boy/man and girl/woman.
+            //Add everything to the caption variable
+
+            CaptionBox.Text = caption;
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
