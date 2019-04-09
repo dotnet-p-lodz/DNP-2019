5dbc52062c7b1cafbf5c2b652e10aba2108082e4
Piotr Ładoński <piolado@gmail.com>
2019-04-10 02:53:18 +0300

    [Post] Json parsing

--- Samples/IntelligentMirror/cs/Pages/ResponsePage.xaml.cs
+++ Samples/IntelligentMirror/cs/Pages/ResponsePage.xaml.cs
@@ -50,17 +50,17 @@ namespace IntelligentMirror.Pages
                     Img.RenderTransformOrigin = new Point(0.5, 0.5);
                     Img.RenderTransform = new ScaleTransform { ScaleX = -1 };
                     string jsonResponse = await azure.MakeAnalysisRequest(stream);
-                    //TODO: (1) Face can be created From Json by providing JSON string as argument. There can be several faces, but now just pick First Or Default one.
-                    //Default means that face variable will be null if there are no faces on the photo.
-                    Face face = Face.??.??;
-                    //TODO: (2) Check, if there are any faces on the photo
-                    if (??)
+
+                    Face face = Face.FromJson(jsonResponse).FirstOrDefault();
+                    if (face != null)
                     {
-                        //TODO: (3) Here use functions which will Setup Response and Setup Caption textboxes. Give them face as the argument.
+                        SetupResponse(face);
+                        SetupCaption(face);
                     }
                     else
                     {
-                        //TODO: (4) If there are no faces, provide some default text for the user in both textboxes, ex. "No faces found"
+                        ResponseBox.Text = "No faces found";
+                        CaptionBox.Text = "No faces found";
                     }
                 }
             }
@@ -68,9 +68,9 @@ namespace IntelligentMirror.Pages
 
         private void SetupResponse(Face face)
         {
-            //TODO: (5) Extract Age, Gender and Glasses information from face.FaceAttributes and add them to the response text.
-            string response = $"Age: {16 /*replace this!*/}\n";
-            response += $"Glasses: {false /*replace this!*/}\n";
+            string response = $"Age: {face.FaceAttributes.Age}\n";
+            response += $"Gender: {face.FaceAttributes.Gender}\n";
+            response += $"Glasses: {face.FaceAttributes.Glasses}\n";
             ResponseBox.Text = response;
         }
 
@@ -78,10 +78,19 @@ namespace IntelligentMirror.Pages
         {
             string caption = $"{face.FaceAttributes.Age}-year-old ";
 
-            //TODO: (6) Extract Gender and Glasses information, but present them in nice, human-readable way
-            //Ex. "22-year-old man wearing glasses" or "19-year-old woman".
-            //Try taking gender and age into account to write boy/man and girl/woman.
-            //Add everything to the caption variable
+            if (face.FaceAttributes.Gender == "male")
+            {
+                caption += "man ";
+            }
+            else
+            {
+                caption += "woman ";
+            }
+
+            if (face.FaceAttributes.Glasses != "NoGlasses")
+            {
+                caption += "wearing glasses";
+            }
 
             CaptionBox.Text = caption;
         }
